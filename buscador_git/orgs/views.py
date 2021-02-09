dee07822d8817d3bfdd22ca2e2ba46298b4bda01
doginnn <diogenesemmanuel@gmail.com>
2021-02-09 15:37:35 -0300

    [UPD]Atualizando a views.py

--- buscador_git/orgs/views.py
+++ buscador_git/orgs/views.py
@@ -6,3 +6,26 @@ from buscador_git.orgs.models import Empresa
 class EmpresaViewSet(viewsets.ModelViewSet):
     serializer_class = EmpresaSerializer
     queryset = Empresa.objects.all()
+
+
+# from github import Github
+#
+# ACCESS_TOKEN = 'put your token here'
+#
+# g = Github(ACCESS_TOKEN)
+#
+#
+# def search_github(keywords):
+#     query = '+'.join(keywords) + '+in:readme+in:description'
+#     result = g.search_repositories(query, 'stars', 'desc')
+#
+#     print(f'Found {result.totalCount} repo(s)')
+#
+#     for repo in result:
+#         print(f'{repo.clone_url}, {repo.stargazers_count} stars')
+#
+#
+# if __name__ == '__main__':
+#     keywords = input('Enter keyword(s)[e.g python, flask, postgres]: ')
+#     keywords = [keyword.strip() for keyword in keywords.split(',')]
+#     search_github(keywords)
